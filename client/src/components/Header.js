e46413768f2f67e51ba2a0c625eb59797b977c2b
PranayKamineny <pranay.kamineny@gmail.com>
2024-07-24 14:09:16 -0400

    testing git again

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Header({page}) {
     return (
       <div className="p-8 justify-between flex sm:flex-col sm:gap-5">
-        <h1 className="text-tertiary text-4xl sm:text-2xl">PRANAYKAMINENY</h1>
+        <h1 className="text-tertiary text-4xl sm:text-2xl">PRANAY KAMINENY</h1>
         <div className="p-2 justify-between flex space-x-10 sm:space-x-4">
             <Link to="/">
                 <h1 className={`text-xl sm:xl ${page === "intro" ? 'text-black' : 'text-gray-500'} cursor-pointer hover:text-black`}>Intro</h1>
