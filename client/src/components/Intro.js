51c01f237f26ed185717621059bb24d4e2dd70e8
PranayKamineny <pranay.kamineny@gmail.com>
2024-12-18 16:22:24 -0500

    ms first semester update

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -3,9 +3,8 @@
 import logo from "../images/logo.jpeg"
 
 function Intro() {
+    let hw_skills = ["C", "C++", "RTOS", "Verilog/VHDL", "SystemVerilog", "FPGA Design/Verification"]
     let sw_skills = ["Python", "Java", "Golang", "React", "Express", "MongoDB", "Docker"];
-    let hw_skills = ["C/C++", "Assembly", "Verilog/VHDL", "SystemVerilog", "Digital Logic"]
-
 
     return (
       <div className="px-5 py-5">
@@ -13,15 +12,28 @@ function Intro() {
             <img className="h-48 w-48 md:h-32 md:w-32" src={logo} alt="[Picture of me]"/>
             <div className="flex flex-col gap-5 py-5 sm:py-0">
                 <p className="text-xl sm:text-base">
-                    Welcome to my page! I'm Pranay, an ECE Master's student at Georgia Tech studying ML.
-                    I've previously worked as a Software Engineer Intern at PayPal, where I built a full stack network topology application.
+                    Welcome to my page! I'm Pranay, an ECE Master's student at Georgia Tech.
+                    I'm currently a GRA at the GT Cyber-Physical Security Lab where I write firmware for MCUs and FPGAs for vehicle security.
+                    I was previously a Software Engineer Intern at PayPal, where I built a full stack network topology application.
                 </p>
                 <p className="text-xl sm:text-base">
-                    I enjoy working on everything from embedded systems to high-level software development.
+                    I'm very passionate about FPGA design/verification, firmware, and software development!
                 </p>
             </div>
         </div>
         <div className='h-[4px] w-full bg-gray-300'></div>
+
+        <div className="py-2">
+            <h1 className="text-xl sm:text-base">Hardware/Embedded Skills</h1>
+            <div className="flex flex-wrap gap-10 mt-5 justify-center sm:gap-4">
+                {hw_skills.map((elem) => (
+                    <div className="border border-primary px-5 py-3 hover:bg-primary group">
+                        <h1 className="text-primary text-xl sm:text-base group-hover:text-white">{elem}</h1>
+                    </div>
+                ))}
+            </div>
+            
+        </div>
 
         <div className="py-2">
             <h1 className="text-xl sm:text-base">Software Skills</h1>
@@ -35,17 +47,7 @@ function Intro() {
         </div>
         
 
-        <div className="py-2">
-            <h1 className="text-xl sm:text-base">Hardware/Embedded Skills</h1>
-            <div className="flex flex-wrap gap-10 mt-5 justify-center sm:gap-4">
-                {hw_skills.map((elem) => (
-                    <div className="border border-primary px-5 py-3 hover:bg-primary group">
-                        <h1 className="text-primary text-xl sm:text-base group-hover:text-white">{elem}</h1>
-                    </div>
-                ))}
-            </div>
-            
-        </div>
+       
       </div>
     );
   }
--- client/src/resources/experience.js
+++ client/src/resources/experience.js
@@ -1,27 +1,36 @@
+// "Developing Intrusion Detection System (IDS) for CAN network security on Hyundai Vehicles. Implemented with C/C++ and Verilog on ARM MCUs and FPGA, and debugged with logic analyzer and oscilloscope."
+
 export const experience = [
     {
         id: 1,
+        company: "CPSec Lab",
+        title: "Graduate Research Assistant",
+        period: "October 2024 - Present",
+        description: "Developing Intrusion Detection System (IDS) for CAN network security on Hyundai Vehicles. Simulated network with multiple ARM MCUs programmed with C++ that periodically send CAN messages to eachother. Implementing IDS with C and Verilog on an FPGA and debugging with oscilloscope and digital logic analyzer. "
+    },
+    {
+        id: 2,
         company: "PayPal",
         title: "Software Engineer Intern",
         period: "May 2023 - August 2023",
         description: "Worked on the Global Network Services(GNS) team to develop a full stack network topology application to swiftly find the root cause of network faults and sped up recovery by 30%. Used React.js for frontend, Golang for backend, Redis Stack database, and Docker/Kubernetes/Jenkins for deployment."
     },
     {
-        id: 2,
+        id: 3,
         company: "Oceaneering",
         title: "Software Engineer Intern",
         period: "May 2022 - January 2023",
         description: "Wrote embedded software to control disposable subsea surveillance vehicles for the United States Navy. Created a publish-subscribe data sharing architecture using .NET applications with C# to link two DDS (data distribution service) implementations to yield a Linux to Windows cross-platform system. Leveraged Linux system administration to run and regulate vehicle software."
     },
     {
-        id: 3,
+        id: 4,
         company: "Terps Racing",
         title: "Low Voltage Team Member",
         period: "March 2021 - April 2022",
         description: "Oversaw development of Heads-Up Display to report real-time speed, temperature, and battery data from sensors using C++ and Python with CAN (controller area network) protocol. Applied embedded software and integrated circuits to optimize race car electrical system."
     },
     {
-        id: 4,
+        id: 5,
         company: "UMD RCPS Lab",
         title: "Software Engineer Intern",
         period: "June 2019 - September 2019",
